36eb1a9cfcfc6f963db74f75ed809f465f2db837
Val <valentin.jeudy@gmail.com>
2021-05-04 23:48:42 +0200

    minor changes

--- index.js
+++ index.js
@@ -232,13 +232,13 @@ module.exports = {
     };
 
     self.apiKeyMiddleware = function(req, res, next) {
-
       if (req.url.substr(0, self.endpoint.length + 1) !== (self.endpoint + '/')) {
         return next();
       }
 
-      var key = req.query.apikey || req.query.apiKey || getAuthorizationApiKey();
-      var taskReq;
+      const key = req.query.apikey || getAuthorizationApiKey();
+
+      let taskReq;
       if (!key) {
         return next();
       }
@@ -247,11 +247,13 @@ module.exports = {
         taskReq = self.apos.tasks.getReq();
         req.user = taskReq.user;
         req.csrfExempt = true;
+
         return next();
       } else {
         var module = _.find(self.registeredModules, function(module) {
           return _.includes(module.options.apiKeys, key);
         });
+
         if (module) {
           taskReq = self.apos.tasks.getReq();
           req.user = taskReq.user;
@@ -264,6 +266,7 @@ module.exports = {
             req.user._permissions['admin-' + module.name] = true;
           }
           req.csrfExempt = true;
+
           return next();
         }
       }
